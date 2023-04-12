3a29026acad7044f1b3f3435d20884ce548a165f
EC2 Default User <ec2-user@ip-172-31-26-189.eu-west-1.compute.internal>
2023-04-12 17:54:33 +0000

    changes in before_install.sh

--- assignementproj/scripts/before_install.sh
+++ assignementproj/scripts/before_install.sh
@@ -1,8 +1,35 @@
-# update os & install python3
-sudo yum -y update
-sudo amazon-linux-extras install -y python3
-sudo yum -y install python3-devel python3-pip
-pip3 install --user --upgrade virtualenv
+#!/usr/bin/env bash
+#clean codedeploy-agent files for a fresh install
 
-# delete app
+sudo rm -rf /home/ec2-user/install
+
+#install CodeDeploy agent
+
+#sudo yum -y update
+
+#sudo yum -y install ruby
+
+#sudo yum -y install wget
+
+#cd /home/ec2-user
+
+#wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+
+#sudo chmod +x ./install
+
+#sudo ./install auto
+
+#update os & install python3
+
+sudo yum update -y
+
+sudo yum install -y python3 python3-pip  python3-devel
+
+pip3 install --user --upgrade virtualenv
+#delete app
+
 sudo rm -rf /home/ec2-user/assignementproj
+
+
+
+
